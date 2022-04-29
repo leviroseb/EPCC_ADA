2b6177ee6b2b07a997123314bd8b84642c63151d
Jhon Flores Pacheco <38104862+leviroseb@users.noreply.github.com>
2022-04-29 15:52:50 -0500

    desarrollo2

--- EjerciciosClase/Semana03/maxProduct.cpp
+++ EjerciciosClase/Semana03/maxProduct.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxProduct(A){
-    for (int i;1;i++){
-        
+int maxProduct(int A[],int n){
+    int max=0;
+    for (int i=1;i<n-1;i++){
+        if(A[i]>=A[i+1]){
+            swap(A[i],A[i+1]);
+        }
+        else if(A[i]<=A[i]){
+            swap(A[i],A[i-1]);
+        }
     }
+
+    return A[n-1]*A[n-2];
 }
 
 
 int main(){
+    int n,A[100],producto;
+    cout<<"Tamaño del arreglo: ";
+    cin>>n;
+    for (int i=0;i<n;i++){
+        cout<<"Añadir: ";
+        cin>>A[i];
+    }
+
+    producto=maxProduct(A,n);
+    print(A);
+
+    cout<<"Producto máximo: "<<producto;
+    return 0;
 
 }
